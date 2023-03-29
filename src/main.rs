ba1f56c69f6b0e8cdf727dc415e5ee27489d54e4
Nat Standridge <standridgen@gmail.com>
2023-03-28 18:35:25 -0600

    Got starting points generated and the Resource for Trajectories established.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .run();
-}
+#[derive(Resource)]
+struct Trajectories(Vec<Vec<Vec3>>); // for storing starting points and their trajectories
+/*  
+    Vec[
+        Vec[ // starting point 1
+            Vec3[0.0, 0.0, 0.0],
+            Vec3[0.0, 0.0, 0.0] // point added as the next point in the line based on starting point
+            ]
+        Vec[ // starting point 2
+            Vec3[0.0, 0.0, 0.0]
+            ]
+        Vec[ // starting point 3, etc...
+
+            ]
+        ]
+    */
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut trajectories: ResMut<Trajectories>
 ) {
 
-    let material_emissive = materials.add(StandardMaterial {
+    // material for sphere
+    let material = materials.add(StandardMaterial {
         base_color: Color::INDIGO,
         metallic: 5.0,
         reflectance: 0.0,
         ..default()
     });
 
+    // mesh for sphere
     let mesh = meshes.add(
         shape::Icosphere {
-            radius: 0.5,
+            radius: 0.1,
             subdivisions: 5,
         }
         .try_into()
@@ -32,27 +45,39 @@ fn setup(
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 12000.0,
+            intensity: 8000.0,
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(0.0, 10.0, 0.0),
+        transform: Transform::from_xyz(0.0, 4.0, 0.0),
         ..default()
     });
     
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
-    commands.spawn(
-        PbrBundle {
-            mesh: mesh.clone(),
-            material: material_emissive.clone(),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..default()
-        }
-    );
+    // looping through to create starting points
+    for z in vec![0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5] {
+        // spawn sphere from cloned mesh
+        commands.spawn(
+            PbrBundle {
+                mesh: mesh.clone(),
+                material: material.clone(),
+                transform: Transform::from_xyz(0.0, 0.0, z),
+                ..default()
+            }
+        );
+        trajectories.0.push(vec![Vec3::new(0.0,0.0,z)]); // Vec3 added to trajectory within Vec[Vec[]]
+    }
+}
 
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Trajectories(Vec::new()))
+        .add_startup_system(setup)
+        .run();
 }
