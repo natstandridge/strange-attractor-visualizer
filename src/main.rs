6a94b2fc309ec5d150922a139a768fec5e2da829
Nat Standridge <standridgen@gmail.com>
2023-03-28 20:55:19 -0600

    Spheres are now animated along attractor paths.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::Rng;
 
 #[derive(Resource)]
 struct Trajectories(Vec<Vec<Vec3>>); // for storing starting points and their trajectories
@@ -17,6 +18,9 @@ struct Trajectories(Vec<Vec<Vec3>>); // for storing starting points and their tr
         ]
     */
 
+#[derive(Resource)]
+struct PointMarker(shape::Icosphere);
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -27,8 +31,8 @@ fn setup(
     // material for sphere
     let material = materials.add(StandardMaterial {
         base_color: Color::INDIGO,
-        metallic: 5.0,
-        reflectance: 0.0,
+        // metallic: 5.0,
+        // reflectance: 0.0,
         ..default()
     });
 
@@ -43,41 +47,137 @@ fn setup(
     );
     
     // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 8000.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(0.0, 4.0, 0.0),
-        ..default()
-    });
+    // commands.spawn(PointLightBundle {
+    //     point_light: PointLight {
+    //         intensity: 8000.0,
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     transform: Transform::from_xyz(0.0, 4.0, 0.0),
+    //     ..default()
+    // });
     
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-50.0, 50.0, 50.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
+    let mut rng = rand::thread_rng();
+
     // looping through to create starting points
-    for z in vec![0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5] {
+    for _ in 0..5 {
+
+        let pos: Vec3 = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0));
+
         // spawn sphere from cloned mesh
         commands.spawn(
             PbrBundle {
                 mesh: mesh.clone(),
                 material: material.clone(),
-                transform: Transform::from_xyz(0.0, 0.0, z),
+                transform: Transform::from_xyz(pos.x, pos.y, pos.z),
                 ..default()
             }
         );
-        trajectories.0.push(vec![Vec3::new(0.0,0.0,z)]); // Vec3 added to trajectory within Vec[Vec[]]
+        trajectories.0.push(vec![Vec3::new(pos.x, pos.y, pos.z)]); // Vec3 added to trajectory within Vec[Vec[]]
+
     }
+}
+
+fn get_next_point(input_point: &Vec3) -> Vec3 {
+    let rho: f32  = 25.0;
+    let sigma: f32 = 10.0;
+    let beta: f32 = 8.0/3.0;
+
+    let dx: f32 = sigma*(input_point.y - input_point.x);
+    let dy: f32 = input_point.x*(rho - input_point.z) - input_point.y;
+    let dz: f32 = input_point.x*input_point.y - beta*input_point.z;
+
+    // println!("dx: {}, dy: {}, dz: {}", dx, dy, dz);
+
+    let sf = 0.01;
+
+    Vec3::new(input_point.x+sf*dx, input_point.y+sf*dy, input_point.z+sf*dz)
+}
+
+fn calculate_and_draw_trajectories(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut trajectories: ResMut<Trajectories>,
+    mut camera: Query<With<Camera3d>>
+    // point_marker: ResMut<PointMarker>
+) {
+
+    // mesh for sphere
+    let mesh = meshes.add(
+        shape::Icosphere {
+            radius: 0.1,
+            subdivisions: 5,
+        }
+        .try_into()
+        .unwrap(),
+    );
+
+    // material for sphere
+    let material = materials.add(StandardMaterial {
+        base_color: Color::INDIGO,
+        metallic: 5.0,
+        reflectance: 0.0,
+        ..default()
+    });
+
+    // println!("trajectories len: {:#?}", trajectories.0.len());
+
+    /* calculate new point according to */
+    let mut comx = 0.0;
+    let mut comy = 0.0;
+    let mut comz = 0.0;
+
+    for trajectory in trajectories.0.iter_mut() {
+        /* get latest point in this trajectory */
+        match trajectory.last() {
+            Some(point) => {
+
+                /* update center of mass calculation */
+                comx += point.x;
+                comy += point.y;
+                comz += point.z;
+
+                /* get the new point for this trajectory */
+                let new_point = get_next_point(point);
+
+                // println!("new_point: {:#?}", new_point);
+
+                /* spawn a sphere there */
+                commands.spawn(
+                    PbrBundle {
+                        mesh: mesh.clone(),
+                        material: material.clone(),
+                        transform: Transform::from_xyz(new_point.x, new_point.y, new_point.z),
+                        ..default()
+                    }
+                );
+
+                trajectory.push(new_point);
+            },
+            None => {}
+        }
+    }
+
+    let num_points = trajectories.0.len() as f32;
+    comx /= num_points;
+    comy /= num_points;
+    comz /= num_points;
+
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(Trajectories(Vec::new()))
+        .insert_resource(PointMarker(shape::Icosphere { radius: 0.1, subdivisions: 5 }.try_into().unwrap()))
         .add_startup_system(setup)
+        .add_system(calculate_and_draw_trajectories)
         .run();
 }
